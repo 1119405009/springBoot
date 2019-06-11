afb8e181e0e3930dd115fe7144adf2d6f45c4ac2
fangzhancai <1119405009@qq.com>
2019-06-11 12:00:00 +0800

    maven打包教程

---
+++ assembly/READNE.md
@@ -0,0 +1,20 @@
+**maven打包**
+
+使用assembly进行打包：
+https://www.cnblogs.com/lianshan/p/7348093.html
+https://www.cnblogs.com/littleatp/p/9278517.html
+http://maven.apache.org/plugins/maven-assembly-plugin/assembly.html
+
+springBoot打包参考
+https://www.cnblogs.com/zhouqinxiong/p/repackage.html
+Spring Boot Maven Plugin
+https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html
+
+Maven内置属性及使用
+https://blog.csdn.net/wangjunjun2008/article/details/17761355
+
+
+
+
+
+
---
+++ assembly/src/main/java/com/felix/assembly/controller/WebController.java
@@ -0,0 +1,26 @@
+package com.felix.assembly.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2017/4/18
+ * Time: 10:14
+ * Description:
+ */
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+
+
+    @RequestMapping
+    public ResponseEntity<?> hello(){
+        return new ResponseEntity<Object>("hello", HttpStatus.OK);
+    }
+}
