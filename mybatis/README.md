41fb8b572ba40ac59e1c8bf946c3f30fbf97c972
fangzhancai <1119405009@qq.com>
2019-06-13 14:15:58 +0800

    mybatis

---
+++ mybatis/README.md
@@ -0,0 +1,8 @@
+spring boot 如何优雅的使用mybatis-spring-boot-starter
+https://blog.csdn.net/zmx729618/article/details/80773887
+
+
+mybatis-spring-boot-autoconfigure
+http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/#
+
+
---
+++ mybatis/src/main/java/com/felix/mybatis/MybatisApplication.java
@@ -0,0 +1,15 @@
+package com.felix.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MybatisApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+
+}
